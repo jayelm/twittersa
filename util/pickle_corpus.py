b6696137e77f66b02e7735098353c52ed3b83786
jmu303 <muj@bc.edu>
2014-12-02 12:10:32 -0500

    Rename corpus to corpora, 100 -> 500
    Because plural. Also, training.100 was improperly named - it actually
    contains 500 examples.
    
    pickle_corpus updated accordingly

--- util/pickle_corpus.py
+++ util/pickle_corpus.py
@@ -8,18 +8,17 @@ import pickle  # Standard pickle for unicode support
 import csv
 
 FILES = [
-    'corpus/testdata.manual.2009.06.14.csv',
-    'corpus/training.100.csv',
-    'corpus/training.1000.csv',
-    'corpus/training.2000.csv',
-    'corpus/training.3000.csv',
-    'corpus/training.4000.csv',
-    'corpus/training.6000.csv',
-    'corpus/training.5000.csv',
-    'corpus/training.7000.csv',
-    'corpus/training.8000.csv',
-    'corpus/training.9000.csv',
-    'corpus/training.10000.csv',
+    'corpora/training.500.csv',
+    'corpora/training.1000.csv',
+    'corpora/training.2000.csv',
+    'corpora/training.3000.csv',
+    'corpora/training.4000.csv',
+    'corpora/training.6000.csv',
+    'corpora/training.5000.csv',
+    'corpora/training.7000.csv',
+    'corpora/training.8000.csv',
+    'corpora/training.9000.csv',
+    'corpora/training.10000.csv',
 ]
 
 
@@ -53,8 +52,8 @@ def serialize(filename, dictionary, verbose=False):
 
 
 def pickle_filename(filename):
-    assert filename[-4:] == '.csv' and filename[:7] == 'corpus/'
-    return 'lib/{}.p'.format(filename[7:-4])
+    assert filename[-4:] == '.csv' and filename[:8] == 'corpora/'
+    return 'lib/{}.p'.format(filename[8:-4])
 
 if __name__ == '__main__':
     for filename in FILES:
