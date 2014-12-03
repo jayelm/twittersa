39b1824b77cb61f4384528e4243f6ac59cff1214
jmu303 <muj@bc.edu>
2014-12-02 22:18:52 -0500

    Rebuild corpora (rows shuffled), rename to pickle
    Shuffling rows ensures more reliable accuracy results since we're not
    just adding an extra couple thousand rows to each training example and
    evaluating performance based on that alone.
    
    .pickle instead of .p because GitHub is misinterpreting .p, and .pickle
    is less ambiguous anyways.
    
    I uploaded the .sh file used to create the samller corpora as well.

---
+++ corpora/create_smaller_corpora.sh
@@ -0,0 +1,7 @@
+sizes=( 100 250 500 750 1000 2500 5000 7500 10000 15000 20000 25000 )
+for i in "${sizes[@]}"; do
+    shuf -o ./training.1600000.processed.noemoticon.csv.shuffled < ./training.1600000.processed.noemoticon.csv.shuffled
+    fout="./training.$i.csv"
+    echo "Writing to $fout"
+    tail -n "$i" ./training.1600000.processed.noemoticon.csv.shuffled > "$fout"
+done
